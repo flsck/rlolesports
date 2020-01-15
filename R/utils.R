79a742a0cb7b2dfcbfa0f5df42b56c0abe713be9
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-01-15 23:23:17 +0100

    Added necessary imports jsonlite and httr, moved api key function

--- R/utils.R
+++ R/utils.R
@@ -3,8 +3,9 @@
 #' for internal use only
 #'
 #' @return the x-api-key needed for querying
-#' @export
 get_key <- function() {
   api_key <- "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z"
   return(api_key)
 }
+
+
