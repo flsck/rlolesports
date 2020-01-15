65f6a9bf052123518dfe6c57d863397fa04004c0
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-01-15 22:47:03 +0100

    Added first function skeleton

---
+++ R/get_key.R
@@ -0,0 +1,10 @@
+#' Provides the public Esports API key
+#'
+#' for internal use only
+#'
+#' @return the x-api-key needed for querying
+#' @export
+get_key <- function() {
+  api_key <- "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z"
+  return(api_key)
+}
