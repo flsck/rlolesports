9330ea8007ea7bc6d4ceed67eea7c6fa8494fff6
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-06-14 23:36:35 +0200

    started further exploring getÃLive data, including tidyr now

--- tests/testthat/test-getLive.R
+++ tests/testthat/test-getLive.R
@@ -13,6 +13,37 @@ parsed <- query_result$parsed
 events <- parsed$data$schedule$events
 
 
-events <- data.frame(a = 1:10)
-events[, c("b", "c")] <- data.frame(bb = 11:20, cc = 21:30)
+event_info <- events[,1:5]
+names(event_info)[which(names(event_info) == "id")] <- "event_id"
+
+league_info <- purrr::pluck(events, "league")
+names(league_info)[which(names(league_info) == "id")] <- "league_id"
+
+match_data <- purrr::pluck(events, "match")
+match_strat_info <- purrr::pluck(match_data, "strategy")
+
+matches <- match_data[, which(names(match_data) != "strategy")]
+match_teams_info <- tidyr::unnest_wider(matches, teams, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_name, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_slug, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_code, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_image, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_result, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_record, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_result_outcome, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_result_gameWins, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_record_wins, names_sep = "_") %>%
+  tidyr::unnest_wider(teams_record_losses, names_sep = "_")
+
+names(match_teams_info)[which(names(match_teams_info) == "id")] <- "match_id"
+
+final_df <- cbind(
+  event_info, league_info, match_strat_info, match_teams_info
+)
+
+names(final_df)[stringr::str_detect(names(final_df), "teams_")] <-
+  names(final_df)[stringr::str_detect(names(final_df), "teams_")] %>%
+  stringr::str_remove("teams_") %>%
+  stringr::str_replace("_1", "_team_1") %>%
+  stringr::str_replace("_2", "_team_2")
 
