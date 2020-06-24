7bc316d51817e6a61088ac77da6fc94b57f2249d
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-06-24 20:00:03 +0200

    Create getLive function, relevant test tbd

--- R/getLive.R
+++ R/getLive.R
@@ -3,8 +3,8 @@
 #'
 #' Get information about live events currently going on..
 #'
-#' @param hl string. Locale or language code using ISO 639-1 and ISO 3166-1 alpha-2.
 #' @param save_details logical. Should just a table be returned, or also details about the requests.
+#' @param hl string. Locale or language code using ISO 639-1 and ISO 3166-1 alpha-2.
 #'
 #' @return Returns an object of class `Schedule` if details were saved. Otherwise, a `data.frame`
 #'         of scheduled matches for the queried league.
@@ -23,9 +23,62 @@ getLive <- function(
     hl = hl
   )
 
-  events_list <- list()
+
+  parsed <- query_result$parsed
+  events <- parsed$data$schedule$events
+  # TODO Null check
+  if(length(events) == 0) stop("There are no live events taking place right now.")
+
+  # alternative way of doing this, utilizing tidyr
+  event_info <- events[,1:5]
+  names(event_info)[which(names(event_info) == "id")] <- "event_id"
+
+  league_info <- purrr::pluck(events, "league")
+  names(league_info)[which(names(league_info) == "id")] <- "league_id"
+
+  match_data <- purrr::pluck(events, "match")
+  match_strat_info <- purrr::pluck(match_data, "strategy")
+
+  matches <- match_data[, which(names(match_data) != "strategy")]
+  match_teams_info <- tidyr::unnest_wider(matches, teams, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_name, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_slug, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_code, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_image, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_result, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_record, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_result_outcome, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_result_gameWins, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_record_wins, names_sep = "_") %>%
+    tidyr::unnest_wider(teams_record_losses, names_sep = "_")
+
+  names(match_teams_info)[which(names(match_teams_info) == "id")] <- "match_id"
+
+  final_df <- cbind(
+    event_info, league_info, match_strat_info, match_teams_info
+  )
+
+  names(final_df)[stringr::str_detect(names(final_df), "teams_")] <-
+    names(final_df)[stringr::str_detect(names(final_df), "teams_")] %>%
+    stringr::str_remove("teams_") %>%
+    stringr::str_replace("_1", "_team_1") %>%
+    stringr::str_replace("_2", "_team_2")
 
 
+  return_obj <- structure(
+    list(
+      live_events = final_df,
+      hl = hl,
+      response = query_result$response
+    ),
+    class = "live"
+  )
+
+  if(save_details) {
+    return(return_obj)
+  } else {
+    return(final_df)
+  }
 
 }
 
