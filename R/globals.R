4b8b6c850c92f6538ef5f52eb45e826c30d0940d
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-06-24 20:18:43 +0200

    Add utilitiy stuff for check passing

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+utils::globalVariables(c("teams", "teams_name", "teams_slug",
+                         "teams_code", "teams_image", "teams_result",
+                         "teams_record", "teams_result_outcome", "teams_result_gameWins",
+                         "teams_record_wins", "teams_record_losses"))
