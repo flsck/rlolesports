04b45c457823755dc89cbc7f63802f6e6966faa0
FlorianSck <47120509+FlorianSck@users.noreply.github.com>
2020-08-16 15:41:34 +0200

    Some additions of files, dependencie version update, that stuff

--- R/getGames.R
+++ R/getGames.R
@@ -2,4 +2,21 @@
 #'
 #' TODO: late prio
 
+getGames <- function(gameId,
+                     save_details = FALSE,
+                     hl = "en-US") {
+  if(!(hl %in% valid_locales())) stop("hl is not valid.")
+  key <- get_apikey()
+  url <- paste0(league_url(), "getGames")
+  query_result <- query_api(
+    url = url,
+    key = key,
+    id = gameId,
+    hl = hl
+  )
 
+
+  parsed <- query_result$parsed
+}
+
+gameId <- "104174613333860709"
