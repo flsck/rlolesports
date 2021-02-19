7c3f0d4ec5ca2b5608173dbd8571bb7f46671719
flsck <florian@sckade.de>
2021-02-19 08:01:27 +0100

    build construct around getWindow

--- R/getWindow.R
+++ R/getWindow.R
@@ -3,18 +3,35 @@
 #'
 #' @inheritParams getGames
 getWindow <- function(gameId,
-                      hl = "en-US") {
+                      hl = "en-US",
+                      startingTime = NULL,
+                      save_details = FALSE) {
+  hl = "en-US"
   if(!(hl %in% valid_locales())) stop("hl is not valid.")
   key <- get_apikey()
   url <- paste0("https://feed.lolesports.com/livestats/v1/window/", gameId)
 
-  query_result <- query_api(
-    url = url,
-    key = key,
-    hl = hl
-  )
+  startingTime <- "2020-06-12T16:19:00Z"
 
-  query_result$parsed$gameMetadata
+
+  if(is.null(startingTime)) {
+    query_result <- query_api(
+      url = url,
+      key = key,
+      hl = hl
+    )
+  } else {
+    query_result <- query_api(
+      url = url,
+      key = key,
+      hl = hl,
+      startingTime = startingTime
+  )
+  }
+
+  query_result$parsed$frames$blueTeam %>%
+    tidyr::unnest_wider("dragons", names_sep = "_") %>%
+    tidyr::unnest_wider("participants", names_repair = "unique")
 
   return()
 
