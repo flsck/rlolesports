6517cc87a85fdccf76a53b6924a48fe103f638a7
flsck <florian@sckade.de>
2021-03-02 21:40:35 +0100

    finish and clean (a bit) getWindow function

--- R/getWindow.R
+++ R/getWindow.R
@@ -39,35 +39,35 @@ getWindow <- function(gameId,
   blue_team <- query_result$parsed$frames$blueTeam
   blue_team$timestamp <- query_result$parsed$frames$rfc460Timestamp
   row.names(blue_team) <- NULL
+  blue_team <- dplyr::distinct(blue_team, timestamp, .keep_all = TRUE)
 
   red_team <- query_result$parsed$frames$redTeam
   red_team$timestamp <- query_result$parsed$frames$rfc460Timestamp
   row.names(red_team) <- NULL
+  red_team <- dplyr::distinct(red_team, timestamp, .keep_all = TRUE)
 
   blue_team_long <- blue_team %>%
     tidyr::unnest_longer("participants", names_repair = "unique")
   blue_team_members <- cbind(
     data.frame(timestamp = blue_team_long$timestamp),
     blue_team_long %>% dplyr::pull("participants") %>% dplyr::rename(participantGold = totalGold)
-    )
-
-  red_team_members <- red_team %>%
-    tidyr::unnest_longer("participants", names_repair = "unique") %>%
-    dplyr::pull("participants") %>%
-    dplyr::rename(participantGold = totalGold)
-
-  blue_team <- cbind(
-    blue_team[, c("timestamp", "totalGold", "inhibitors", "towers", "barons", "totalKills", "dragons")],
-    blue_team_members
   )
-  red_team <- cbind(
-    red_team[, c("timestamp", "totalGold", "inhibitors", "towers", "barons", "totalKills", "dragons")],
-    red_team_members
-  )
+  blue_team_joined <- dplyr::right_join(blue_team, blue_team_members, by = "timestamp")
+  blue_team_joined$team <- "blue"
 
-  both_teams <- rbind(blue_team, red_team)
+  red_team_long <- red_team %>%
+    tidyr::unnest_longer("participants", names_repair = "unique")
+  red_team_members <-  cbind(
+    data.frame(timestamp = red_team_long$timestamp),
+    red_team_long %>% dplyr::pull("participants") %>% dplyr::rename(participantGold = totalGold)
+  )
+  red_team_joined <- dplyr::right_join(red_team, red_team_members, by = "timestamp")
+  red_team_joined$team <- "red"
+
+  both_teams <- rbind(blue_team_joined, red_team_joined)
   both_teams <- dplyr::left_join(both_teams, team_data, by = c("participantId" = "participantId"))
 
-  return()
+  return(list(window = both_teams,
+              metaData = meta_data))
 
 }
