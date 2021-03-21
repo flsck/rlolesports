aa3f5eb15eb7ae906383f46624157374cb7d2eea
flsck <florian@sckade.de>
2021-03-21 22:57:15 +0100

    finish wrapper functions for complete game information (getWindow and getDetails)

---
+++ tests/testthat/test-getCompleteWindow.R
@@ -0,0 +1,27 @@
+#
+
+# It's not testing rn due to long time for a query.
+
+set_apikey()
+#
+# This is supposed to be an explanatory workflow.
+# TODO: Remove this and move into a vignette.
+leagues <- getLeagues(save_details = TRUE)
+lec_id <- leagues$content %>%
+  dplyr::filter(name == "LEC") %>%
+  dplyr::select(id)
+lec_id <- lec_id$id
+tourney <- getTournamentsForLeague(leagueId = lec_id)
+lec_spring_id <- tourney %>%
+  dplyr::filter(slug == "lec_2021_split1") %>%
+  dplyr::select(id) %>% dplyr::pull(1)
+lec_standings <- getStandings(lec_spring_id)
+lec_schedule <- getSchedule(lec_id)
+g2_mad_id <- lec_schedule$match.id[1]
+match_example <- getEventDetails(g2_mad_id)
+g2_mad_game <- match_example$games$game_id[1]
+# same as
+g2_mad_game <- "104169295283008519"
+
+g2m <- getCompleteWindow(g2_mad_game)
+
